d629eb6a05dfa4897a1bed455add881d108061ef
Liaxs <elias.saavedra.lol@gmail.com>
2017-06-02 06:03:00 -0300

    prueba 1

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(function main() {
+	function $(e){
+		return document.querySelector(e);
+	}
+	const AUTH = firebase.auth();
+	$("#btnLogin").addEventListener('click', e => {
+		let email = $('#email').value;
+		let pass = $('#password').value;
+    	const promise = AUTH.signInWithEmailAndPassword(email, pass);
+    	promise.catch(e => alert(e.message));
+	});
+	$("#btnSingUp").addEventListener('click', e => {
+		let email = $('#email').value;
+		let pass = $('#password').value;
+    	const promise = AUTH.createUserWithEmailAndPassword(email, pass);
+    	promise.catch(e => alert(e.message));
+	});
+	AUTH.onAuthStateChanged(firebaseUser =>{
+    if (firebaseUser) {
+      console.log(firebaseUser);
+      //window.location.href = "dashboard.html";
+    }
+    else{
+      alert("No logeado");
+    }
+  });
+	
+})();
